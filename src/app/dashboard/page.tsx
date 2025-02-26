4757abda0f3067b03f304df5359312a966144393
ukeem <ikim.trader@gmail.com>
2025-02-26 21:28:12 +0500

    Update page.tsx

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,24 +1,43 @@
-'use client'
+'use client';
 import AdminPanel from '@/components/AdminPanel';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getLocal } from '@/lib/fn';
-import { useCars } from '@/context/CarsContext';
+import { useCarStore } from '@/store/useCarStore';
+import { Car } from '@/types/Car';
+import Loading from '../loading';
 
 export default function Dashboard() {
-	// const [loading, setLoading] = useState(true);
-	const { cars } = useCars();
+	const { cars, setCars } = useCarStore();
+	const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
-
-		const role = getLocal('role')
+		const role = getLocal('role');
 		if (role !== 'ADMIN' || !role) {
-			window.location.href = '/'
+			window.location.href = '/';
 		}
-	}, []); // ✅ Добавляем пустой массив зависимостей, чтобы useEffect вызывался 1 раз
+	}, []);
 
-	// if (loading) {
-	// 	return <Loading />; // ✅ Возвращаем компонент, а не просто пустой JSX
-	// }
+	useEffect(() => {
+		const fetchCars = async () => {
+			try {
+				const response = await fetch("/response.json");
+				if (!response.ok) {
+					throw new Error('Network response was not ok');
+				}
+				const data: Car[] = await response.json();
+				setCars(data);
+			} catch (error) {
+				console.error('Failed to fetch cars:', error);
+			} finally {
+				setLoading(false);
+			}
+		};
+		fetchCars();
+	}, []);
+
+	if (loading) {
+		return <Loading />;
+	}
 
 	return <AdminPanel allCars={cars} />;
 }
