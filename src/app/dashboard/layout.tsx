f31ac828271fe37a2357ad98e1ead9262dc1be92
ukeem <ikim.trader@gmail.com>
2025-02-26 22:28:54 +0500

    Update layout.tsx

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./globals.css";
+import "../globals.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
