853df5f6b0964b3ed301e100a0c31f50c702e576
ukeem <ikim.trader@gmail.com>
2025-02-27 15:45:58 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ import { getCars } from '@/api/cars';
 import { CarsProvider } from '@/context/CarsContext';
 import FilterBtn from '@/components/FilterBtn';
 import { Metadata } from 'next';
-import { ImageResponse } from "next/og";
 import Script from 'next/script';
 
 export const metadata: Metadata = {
@@ -35,30 +34,30 @@ export const metadata: Metadata = {
 	},
 };
 
-export const ogImage = async () => {
-	return new ImageResponse(
-		(
-			<div
-				style={{
-					fontSize: 50,
-					background: "black",
-					color: "white",
-					width: "100%",
-					height: "100%",
-					display: "flex",
-					alignItems: "center",
-					justifyContent: "center",
-				}}
-			>
-				Авто из Кореи
-			</div>
-		),
-		{
-			width: 1200,
-			height: 630,
-		}
-	);
-};
+// export const ogImage = async () => {
+// 	return new ImageResponse(
+// 		(
+// 			<div
+// 				style={{
+// 					fontSize: 50,
+// 					background: "black",
+// 					color: "white",
+// 					width: "100%",
+// 					height: "100%",
+// 					display: "flex",
+// 					alignItems: "center",
+// 					justifyContent: "center",
+// 				}}
+// 			>
+// 				Авто из Кореи
+// 			</div>
+// 		),
+// 		{
+// 			width: 1200,
+// 			height: 630,
+// 		}
+// 	);
+// };
 
 export const revalidate = 3600
 
