30a8349b8735504661368f17247f24f7bbd3e62c
ukeem <ikim.trader@gmail.com>
2025-03-01 05:57:16 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -19,6 +19,9 @@ const nextConfig: NextConfig = {
         ],
         dangerouslyAllowSVG: true,
     },
+    serverRuntimeConfig: {
+        apiTimeout: 3600000, // 1 час
+    },
     // eslint: {
     //     // Warning: This allows production builds to successfully complete even if
     //     // your project has ESLint errors.
