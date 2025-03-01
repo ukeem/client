8f4636e5c80344ab59fd6062dd69e106735631f3
ukeem <ikim.trader@gmail.com>
2025-03-01 15:08:23 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,8 +52,38 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 	return (
 
 		<html lang="ru">
+			<head>
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) { return; }
+              }
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(100089387, "init", {
+                  clickmap:true,
+                  trackLinks:true,
+                  accurateTrackBounce:true,
+                  webvisor:true
+              });
+            `,
+					}}
+				/>
+				<noscript>
+					<div>
+						<img
+							src="https://mc.yandex.ru/watch/100089387"
+							style={{ position: "absolute", left: "-9999px" }}
+							alt=""
+						/>
+					</div>
+				</noscript>
+			</head>
 			<body className={inter.className}>
-				<YandexMetrika />
 				{children}
 				<DangerBtn />
 			</body>
